9fdf03d129f5ead0af8085d2375a6f96687f90c6
RacoonGang <gudtjr0820@naver.com>
2021-03-03 07:19:00 +0900

    Update script.js
    script.js has been updated

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // Here we go!
+
+class Block {
+    constructor(index, timestamp, data, previousHash = '') {
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.hash = this.calculateHash();
+        this.nonce = 0;
+    }
+
+    calculateHash() {
+        return SHA256;
+    }
+}
