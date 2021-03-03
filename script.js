2ad02306b0bb35b55ce3eece6241107319b96de8
RacoonGang <gudtjr0820@naver.com>
2021-03-03 11:05:19 +0900

    updated

--- script.js
+++ script.js
@@ -3,7 +3,30 @@
 const SHA256 = require('crypto-js/sha256');
 
 class Patient_Info {
-    
+    constructor(name, age, sex, dateOfBirth, height, weight, bloodType, address, phoneNum, country){
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+        this.dateOfBirth = dateOfBirth;
+        this.height = height;
+        this.weight = weight;
+        this.bloodType = bloodType;
+        this.address = address;
+        this.phoneNum = phoneNum;
+        this.country = country;
+        return this;
+    }
+
+    createPatient(name, age, sex, dateOfBirth, height, weight, bloodType, address, phoneNum, country) {
+        // return new Patient_Info(name, age, sex, dateOfBirth, height, weight, bloodType, address, phoneNum, country);
+        let T_patient = new Patient_Info(name, age, sex, dateOfBirth, height, weight, bloodType, address, phoneNum, country);
+        alert(T_patient);
+
+        // Testing
+        let name = document.getElementById('name').value;
+        alert(name);
+
+    }
 }
 
 class Block {
@@ -11,10 +34,11 @@ class Block {
     // timestamp -> when the block was created
     // data -> Any data that we want to associate with this block
     // previousHash -> string that contains the hash of the block before this one (*important*)
-    constructor(index, timestamp, data, previousHash = '') {
+    constructor(index, timestamp, data, accessibility, previousHash = '') {
         this.index = index;
         this.timestamp = timestamp;
         this.data = data;
+        this.accessibility = accessibility;
         this.previousHash = previousHash;
         this.hash = this.calculateHash();
         this.nonce = 0;
@@ -23,8 +47,8 @@ class Block {
     // Calculate the hash function of this block
     calculateHash() {
         return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
-    }   
-    
+    }
+
     // Proof-of-work algorithm
     mineBlock(difficulty){
         while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")){
@@ -75,8 +99,29 @@ class Blockchain {
 
 let PatientConsent = new Blockchain();
 
-console.log("Mining block 1...");
-PatientConsent.addBlock(new Block(1, "02/03/2021", {amount: 4}));
 
-console.log("Mining block 2...");
-PatientConsent.addBlock(new Block(2, "02/03/2021", {amount: 8}));
+
+/* html
+가입하는 단계
+name:
+age:
+sex:
+.
+.
+phone:
+
+button -submit and next-
+
+accessibility
+yes or no
+yes or no
+yes or no
+yes or no
+yes or no
+yes or no
+
+button -submit and next-
+
+
+<script type="text/javascript" src="practice.js"></script>
+*/
